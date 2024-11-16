a00988ed2194767bb4510a04202dfdda902992f1
veringsek <veringsek@gmail.com>
2024-11-16 09:15:29 +0800

    screenshooter

---
+++ tools/generate-screenshooter.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+let string = fs.readFileSync('diamond.omp.json', { encoding: 'utf8' });
+let replacements = {
+    Host: /\{\{\s*\.HostName\s*\}\}/g,
+    '$1$2Admin{{ else }}User$3': /(\{\{\s*if\s+.Root\s*\}\})([^{]+)(\{\{\s*end\s*\}\}.*)\{\{\s*\.UserName\s*\}\}/g,
+    '$1$2$6Admin$3$4$6User$5': /(\{\{\s*if\s+.Root\s*\}\})([^{]+)(\{\{\s*else\s*\}\})([^{]+)(\{\{\s*end\s*\}\})(.*)\{\{\s*\.UserName\s*\}\}/g
+};
+for (let replacer in replacements) {
+    let pattern = replacements[replacer];
+    string = string.replace(pattern, replacer);
+}
+fs.writeFileSync('tools/diamond.omp.json', string);
