c6cd42ca00720526662457ee28534f8bb5d13d86
veringsek <veringsek@gmail.com>
2024-11-21 07:32:57 +0800

    generate-screenshooter: read accent

--- tools/generate-screenshooter.js
+++ tools/generate-screenshooter.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 
 let string = fs.readFileSync('diamond.omp.json', { encoding: 'utf8' });
+let reads = {
+    accent: string.match(/\"accent_color\":\s*\"(#[a-zA-Z0-9]{6})\"/)[1]
+};
 let replacements = {
     Host: /\{\{\s*\.HostName\s*\}\}/g,
     '$1$2Admin{{ else }}User$3': /(\{\{\s*if\s+.Root\s*\}\})([^{]+)(\{\{\s*end\s*\}\}.*)\{\{\s*\.UserName\s*\}\}/g,
     '$1$2$6Admin$3$4$6User$5': /(\{\{\s*if\s+.Root\s*\}\})([^{]+)(\{\{\s*else\s*\}\})([^{]+)(\{\{\s*end\s*\}\})(.*)\{\{\s*\.UserName\s*\}\}/g,
-    '"tone": "#0099F8"': /"tone":\s*"accent"/g
+    [`"$1": "${reads.accent}"`]: /"([^"]+)":\s*"accent"/g
 };
 for (let replacer in replacements) {
     let pattern = replacements[replacer];
