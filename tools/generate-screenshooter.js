06f79495cc068a022b1108a1f1b1a6064f3cd5c9
veringsek <veringsek@gmail.com>
2024-11-21 07:08:36 +0800

    executiontime threshold 5000

--- tools/generate-screenshooter.js
+++ tools/generate-screenshooter.js
@@ -5,7 +5,7 @@ let replacements = {
     Host: /\{\{\s*\.HostName\s*\}\}/g,
     '$1$2Admin{{ else }}User$3': /(\{\{\s*if\s+.Root\s*\}\})([^{]+)(\{\{\s*end\s*\}\}.*)\{\{\s*\.UserName\s*\}\}/g,
     '$1$2$6Admin$3$4$6User$5': /(\{\{\s*if\s+.Root\s*\}\})([^{]+)(\{\{\s*else\s*\}\})([^{]+)(\{\{\s*end\s*\}\})(.*)\{\{\s*\.UserName\s*\}\}/g,
-    '"back": "#0099F8"': /"back":\s*"accent"/g
+    '"tone": "#0099F8"': /"tone":\s*"accent"/g
 };
 for (let replacer in replacements) {
     let pattern = replacements[replacer];
